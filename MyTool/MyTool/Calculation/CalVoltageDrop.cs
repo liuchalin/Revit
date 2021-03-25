411c04948a0849dccab9e3df1562c3dd8d13aa27
Chalin <181654661@qq.com>
2021-03-25 15:23:38 +0800

    20210325

---
+++ MyTool/MyTool/Calculation/CalVoltageDrop.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using MyTool.View;
+using System.Windows.Interop;
+
+namespace MyTool.Calculation
+{
+    [Transaction(TransactionMode.Manual)]
+    class CalVoltageDrop : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Window_CalVlotageDrop win = new Window_CalVlotageDrop();
+            WindowInteropHelper helper = new WindowInteropHelper(win);
+            helper.Owner = Autodesk.Windows.ComponentManager.ApplicationWindow;
+            win.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            win.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
